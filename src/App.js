2020c7768b835b3713d8e7e599d3e9ee467d4337
Brijbihari24 <55361395+Brijbihari24@users.noreply.github.com>
2021-03-17 00:15:37 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useContext } from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
+import { toast } from "react-toastify";
+import BuyPage from "./Components/BuyPage";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const AddInCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
+      return array.id == item.id;
+
+      if (isAlreadyAdded !== -1) {
+        return toast("already present in the cart", { type: "error" });
+      }
+    });
+
+    setCartItem([...cartItem, item]);
+  };
+
+  const buyNow = () => {
+    setCartItem([]);
+    toast("Purchase complete", {
+      type: "success",
+    });
+  };
+
+  const removeItem = (item) => {
+    setCartItem(cartItem.filter((singleitem) => singleitem.id !== item.id));
+    toast("Item removed", {type:"info"})
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BuyPage AddInCart={AddInCart}/>
     </div>
   );
 }
